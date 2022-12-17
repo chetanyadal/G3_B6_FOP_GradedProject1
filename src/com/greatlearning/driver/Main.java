ec7bc64d80997028e64b4498070b975c072fbb37
Chetan Yadal <chetanyadal@gmail.com>
2022-12-17 20:41:09 +0530

    Initial Commit

---
+++ src/com/greatlearning/driver/Main.java
@@ -0,0 +1,32 @@
+package com.greatlearning.driver;
+
+import com.greatlearning.departments.AdminDepartment;
+import com.greatlearning.departments.HrDepartment;
+import com.greatlearning.departments.TechDepartment;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		AdminDepartment adm=new AdminDepartment();
+		System.out.println("Welcome to "+adm.departmentName());
+		System.out.println(adm.getTodaysWork());
+		System.out.println(adm.getWorkDeadline());
+		System.out.println(adm.isTodayAHoliday()+"\n");
+		
+		HrDepartment hr=new HrDepartment();
+		System.out.println("Welcome to "+hr.departmentName());
+		System.out.println(hr.doActivity());
+		System.out.println(hr.getTodaysWork());
+		System.out.println(hr.getWorkDeadline());
+		System.out.println(hr.isTodayAHoliday()+"\n");
+		
+		TechDepartment tech=new TechDepartment();
+		System.out.println("Welcome to "+tech.departmentName());
+		System.out.println(tech.getTodaysWork());
+		System.out.println(tech.getWorkDeadline());
+		System.out.println(tech.getTechStackInformation());
+		System.out.println(tech.isTodayAHoliday());
+	}
+
+}
